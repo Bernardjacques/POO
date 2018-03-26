fd7a20cf9f307d4165c33f96991e7138bec374d5
Bernardjacques <bernardjacques789@gmail.com>
2018-03-26 15:40:35 +0200

    AddExoublie

--- voiture/home.php
+++ voiture/home.php
@@ -4,24 +4,17 @@ require 'voiture.php';
 require 'date.php';
 
 $audi = new voiture();
-$audi->nom ='Audi';
-//echo $audi->nom;
-$audi->nom();
+$audi->name ='Audi';
+$audi->name();
 $audi->modele();
 $audi->kilometre();
-$audi->poid();
+$audi->weight();
 $audi->immatriculation();
 $audi->datecirculation();
-// $audi->paysbe();
-// $audi->paysfr();
-// $audi->paysde();
-// $audi->couleur();
 echo "<br>-------------<br>";
 
 $mercedes = new voiture("vw", "golf", "champagne", "350 000", "1 Tonne 8");
-$mercedes->nom = 'Vw';
-echo $mercedes->nom;
-
-//var_dump($audi, $mercedes);
+$mercedes->name = 'Vw';
+echo $mercedes->name;
 
 ?>
--- voiture/voiture.php
+++ voiture/voiture.php
@@ -1,96 +1,83 @@
 <?php
-// ini_set('display_errors','on');
-// error_reporting(E_ALL);
-
-//$annee = date("Y");
 
 class voiture {
     
-    public function rouler() {
+    public function rouler() 
+    {
         if(isset($_POST['submit']))
         $kilometre = $kilometre + 100000;
     }
-    
-    public $nom = 'Audi';
+    public $name = 'Audi';
     public $modele ='A4';
-    public $couleur = 'champagne';
+    public $color = 'champagne';
     public $kilometre = 100000;
-    public $poid = 1800;
+    public $weight = 1800;
     public $immatriculation = "1 HPJ 790";
     public $paysbe = "Belgique";
     public $paysfr ="France";
     public $paysde ="Allemagne";
     public $datecirculation ="2002";
 
+    public function __construct($name, $modele, $color, $kilometre, $weight, $immatriculation, $paysbe, $paysfr, $paysde, $datecirculation)
+    {
 
-    public function __construct($nom, $modele, $couleur, $kilometre, $poid, $immatriculation, $paysbe, $paysfr, $paysde, $datecirculation){
-
-        // $this->nom = ucfirst($nom);
-        // $this->modele = $modele;
-        // $this->couleur = $couleur;
-        // $this->kilometre = $kilometre;
-        // $this->poid = $poid;
-        // $this->immatriculation = $immatriculation;
-        // $this->paysbe = $paysbe;
-        // $this->paysfr = $paysfr;
-        // $this->paysde = $paysde;
-        // $this->datecirculation = $datecirculation;
     }
-
-        public function nom() {
-            $nom = "Audi";
-            echo "Ma voiture est une ".$this->nom;
+        public function name() 
+        {
+            $name = "Audi";
+            echo "Ma voiture est une ".$this->name;
             echo "</br>";
         }
 
-        public function modele() {
-            // function couleur(){
-            //     $couleur ="champagne";
-            // }
+        public function modele() 
+        {
             $modele = "A4";
-            echo "Mon Audi est une ".$this->modele . " " . "de couleur" . " " .$this->couleur;
+            echo "Mon Audi est une ".$this->modele . " " . "de color" . " " .$this->color;
             echo "</br>";
         }
 
-        public function kilometre(){ 
+        public function kilometre()
+        { 
             $kilometre =350000;
             echo "Elle a ".$this->kilometre . " " . "kilometre :" . " ";
 
-            if($this->kilometre > 200000) {
+            if($this->kilometre > 200000) 
+            {
                 echo "High";
             }
-            elseif($this->kilometre > 10000) {
+            elseif($this->kilometre > 10000) 
+            {
                 echo "Middle";
             }
-            else {
+            else 
+            {
                 echo "Low";
             }
-            
                 echo "</br>";
         }
-        
-        public function poid() {
-            $poid =1800;
-            echo "Elle pèse". " " .$this->poid . " " . " Kg, donc c'est une voiture ";
-            if($this->poid > 3500) {
+        public function weight() 
+        {
+            $weight =1800;
+            echo "Elle pèse". " " .$this->weight . " " . " Kg, donc c'est une voiture ";
+            if($this->weight > 3500) 
+            {
                 echo "utilitaire";
             }
-            else{
+            else
+            {
                 echo "commerciale";
             }
             echo "</br>";
         }
-
-        public function immatriculation() {
+        public function immatriculation() 
+        {
             echo "Son immatriculation est ".$this->immatriculation . " " . "et elle provient de". " " .$this->paysbe;
             echo "</br>";
         }
-
-        public function datecirculation() {
-            //$date = $annee - $datecirculation;
+        public function datecirculation() 
+        {
             echo "Elle a été mise en circulation en ".$this->datecirculation . " " . "donc elle circule depuis". " " .$this->date;
         }
-
 }
 ?>
 
